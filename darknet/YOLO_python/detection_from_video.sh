a7be68993c5ce795c9bb53d5a951edddbd5ef615
TakakiNishio <t2k4k0h61201@gmail.com>
2018-06-26 16:09:04 +0900

    [modify] new dataset name

--- darknet/YOLO_python/detection_from_video.sh
+++ darknet/YOLO_python/detection_from_video.sh
@@ -1,19 +1,19 @@
 m="15"
 l="carrot"
 
-# carrots L size
-for i in `seq 1 12`
+# carrots group_a
+for i in `seq 1 45`
 do
-    echo video: carrot_l$i
-    python darknet_find_object_from_video.py -v ../../../dataset/carrots/videos/l_size/carrot_l$i.avi -l $l -od ../../../dataset/carrots/detected_by_gcnn/from_videos/l_size/carrot_l$i -m $m
+    echo video: carrot_a$i
+    python darknet_find_object_from_video.py -v ../../../dataset/carrots/videos/group_a/carrot_a$i.avi -l $l -od ../../../dataset/carrots/detected_by_gcnn/from_videos/group_a/carrot_a$i -m $m
     sleep 5
 done
 
-# carrots S size
-for i in `seq 1 12`
+# carrots group_b
+for i in `seq 1 45`
 do
-    echo video: carrot_s$i
-    python darknet_find_object_from_video.py -v ../../../dataset/carrots/videos/s_size/carrot_s$i.avi -l $l -od ../../../dataset/carrots/detected_by_gcnn/from_videos/s_size/carrot_s$i -m $m
+    echo video: carrot_b$i
+    python darknet_find_object_from_video.py -v ../../../dataset/carrots/videos/group_b/carrot_s$i.avi -l $l -od ../../../dataset/carrots/detected_by_gcnn/from_videos/group_b/carrot_b$i -m $m
     sleep 5
 done
 
--- darknet/YOLO_python/detection_from_video_superposition.sh
+++ darknet/YOLO_python/detection_from_video_superposition.sh
@@ -2,7 +2,7 @@ m="15"
 l="carrot"
 
 # carrots group A
-for i in `seq 1 28`
+for i in `seq 1 45`
 do
     echo video: carrot_a$i
     python darknet_find_object_from_video.py -v ../../../dataset/carrots/videos/group_a/carrot_a$i.avi   -l carrot -od ../../../dataset/carrots/detected_by_gcnn/from_videos_superposition/group_a/carrot_a$i -m $m -superposition
@@ -10,7 +10,7 @@ do
 done
 
 # carrots group B
-for i in `seq 1 28`
+for i in `seq 1 45`
 do
     echo video: carrot_b$i
     python darknet_find_object_from_video.py -v ../../../dataset/carrots/videos/group_b/carrot_b$i.avi   -l carrot -od ../../../dataset/carrots/detected_by_gcnn/from_videos_superposition/group_b/carrot_b$i -m $m -superposition
